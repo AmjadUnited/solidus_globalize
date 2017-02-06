026da2fa1637afb16ca86bb4bc7e471e558f00bf
Ismael G Marin <ismael.marin@gmail.com>
2017-02-06 00:22:41 -0600

    Fix undefined method find_by_permalink

---
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -0,0 +1,11 @@
+Spree::TaxonsController.class_eval do
+
+  def show
+    @taxon = Spree::Taxon.find_by(permalink: params[:id])
+    return unless @taxon
+
+    @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
+    @products = @searcher.retrieve_products
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+  end
+end
